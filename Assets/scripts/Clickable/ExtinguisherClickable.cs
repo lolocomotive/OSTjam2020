ced6df687a1f4459d960347e79cd3c45e7f7c7c4
lolocomotive <lolocomotive181@gmail.com>
2021-01-20 20:28:53 +0100

    Finish game

---
+++ Assets/scripts/Clickable/ExtinguisherClickable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtinguisherClickable : MonoBehaviour, Clickable
+{
+    public Inventory inv;
+    public Sprite extinguisherSprite;
+    public Transform player;
+    public float distanceToClick;
+    public int oid;
+
+    public void click()
+    {
+        if (Vector2.Distance(player.position, this.transform.position) < distanceToClick)
+        {
+            inv.addItem(new ExtinguisherItem(extinguisherSprite));
+            Lab.destroyedObjs.Add(oid);
+            StaircaseMovement.hasExt = true;
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inv = InventoryHandler.inv;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
