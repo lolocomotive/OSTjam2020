2971fea4ce5e7c824029fcbb06efeb88d6e03c76
lolocomotive <lolocomotive181@gmail.com>
2021-01-20 20:31:21 +0100

    Finish game

---
+++ Assets/scripts/Scenepersistency/Lab.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lab : MonoBehaviour
+{
+    public Transform obj1;
+    public Transform obj2;
+    public static List<int> destroyedObjs = new List<int>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (int i in destroyedObjs)
+        {
+            switch (i)
+            {
+                case 1:
+                    Destroy(obj1.gameObject);
+                    break;
+                case 2:
+                    Destroy(obj2.gameObject);
+                    break;
+            }
+        }
+    }
+}
